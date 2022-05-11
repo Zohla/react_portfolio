abd9484809b47f86617ca27c4f766c9e7d174dad
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-11 19:53:03 +0200

    Initial commit

---
+++ src/components/Intro.js
@@ -0,0 +1,14 @@
+import "./Intro.css";
+
+function Intro() {
+  return (
+    <section>
+      <p>Hi!</p>
+      <p>
+        I'm<span> Maren,</span>
+      </p>
+      <p>a web developer</p>
+    </section>
+  );
+}
+export default Intro;
---
+++ src/components/Navigation.js
@@ -0,0 +1,16 @@
+import "./Navigation.css";
+
+/* import { Link } from "react-router-dom"; */
+
+function Navigation() {
+  return (
+    <nav>
+      <ul>
+        <li>Projects</li>
+        <li>About</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  );
+}
+export default Navigation;
