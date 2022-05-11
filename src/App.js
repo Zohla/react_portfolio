5581f546e617608338a641441e43c59abd27d952
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-11 22:50:05 +0200

    add .circleCI folder and yml file

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
+import Projects from "./components/Projects";
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
+      <Projects />
 
       <img src={logo} className="App-logo" alt="logo" />
     </div>
---
+++ src/components/Projects.js
@@ -0,0 +1,4 @@
+function Projects() {
+  return <h2>Some things I've made</h2>;
+}
+export default Projects;
