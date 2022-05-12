62b527e83433546a068b275c61741ec0bd8731a5
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-12 15:45:41 +0200

    import Languages

--- src/App.js
+++ src/App.js
@@ -3,12 +3,15 @@ import "./App.css";
 import Header from "./components/Header";
 import Intro from "./components/Intro";
 import Projects from "./components/Projects";
+import Languages from "./components/Languages";
+
 function App() {
   return (
     <div className="App">
       <Header />
       <Intro />
       <Projects />
+      <Languages />
       <img src={logo} className="App-logo" alt="logo" />
     </div>
   );
