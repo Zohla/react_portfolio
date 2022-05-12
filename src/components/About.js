eb192f8304d2ebafc69d99f5d0a2530a534ea245
Maren Lilleberre <marenlilleberre@gmail.com>
2022-05-12 16:13:44 +0200

    add contact and about section

---
+++ src/components/About.js
@@ -0,0 +1,31 @@
+import maren from "../maren.png";
+import "./About.css";
+
+function About() {
+  return (
+    <section id="About">
+      <h3>About</h3>
+      <img
+        src={maren}
+        className="Maren-img"
+        alt="Maren smiling at the camera"
+      ></img>
+      <p>
+        I have been coding since 2010, and after going the 'self-taught' route,
+        I started my formal education as a web developer at Noroff in the fall
+        of 2020.
+      </p>
+      <p>
+        I enjoy exploring and mastering new technologies, but I believe that we
+        should take no shortcuts if it's depriving someone of having access to
+        the things we are building. Therefore, writing semantically correct
+        markup and making the product accessible is important to me. me.
+      </p>
+      <p>
+        In my spare time, I spend time with family and friends, and I am
+        passionate about my many hobbies.
+      </p>
+    </section>
+  );
+}
+export default About;
---
+++ src/components/Contact.js
@@ -0,0 +1,32 @@
+import "./Contact.css";
+
+function Contact() {
+  return (
+    <section id="Contact">
+      <h3>Contact</h3>
+      <p>
+        I'm currently looking for new opportunities, and my{" "}
+        <a href="mailto:marenlilleberre@gmail.com">
+          <span className="Link-hover">inbox</span>{" "}
+        </a>{" "}
+        is always open! You can also contact me on{" "}
+        <a
+          href="https://www.linkedin.com/in/maren-lilleberre-117406188/"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="Link-hover">LinkedIn</span>{" "}
+        </a>{" "}
+        or{" "}
+        <a
+          href="https://twitter.com/MarenLilleberre"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <span className="Link-hover">Twitter</span>.
+        </a>
+      </p>
+    </section>
+  );
+}
+export default Contact;
